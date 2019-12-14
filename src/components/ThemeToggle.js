ddca8e3663f532130d7cf64fc56d899ec1a5adfb
Trung Nguyen <trung1704ptit@gmail.com>
2019-12-14 09:24:14 +0700

    init

---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class ThemeToggle extends Component {
+    static contextType = ThemeContext;
+
+    render() {
+        const { toggleTheme } = this.context;
+
+        return (
+            <button onClick={toggleTheme}>Change Theme</button>
+        );
+    }
+}
+
+export default ThemeToggle;
